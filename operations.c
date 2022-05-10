237ce38b2cc2fcc53b4c5648f0711c7047207fe0
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2022-05-10 16:31:47 +0200

    Create operations.c

---
+++ operations.c
@@ -0,0 +1,18 @@
+#include "push_swap.h"
+
+void p_stack()
+{
+	
+}
+
+void s_stack(t_stack **top)
+{
+	int	tmp;
+	
+	if ((*top) && (*top->next))
+	{
+		tmp = *top->number;
+		*top->number = *top->next->number;
+		*top->next->number = tmp;
+	}
+}
