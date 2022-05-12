393361dbf007da6422aaaa049005a53d026d2b47
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2022-05-12 10:36:41 +0200

    Update operation_r.c

--- operation_r.c
+++ operation_r.c
@@ -1,23 +1,32 @@
 #include "push_swap.h"
 
-void	r_stack()
+void	r_stack(t_stack **a)
 {
+	t_stack	tmp;
 	
+	tmp = (t_stack *)malloc(sizeof(t_stack));
+	if (!tmp)
+		return (NULL);
+	tmp = (*top);
+	while (*top)
+		(*top) = (*top)->next;
+	(*top)->next = tmp;
+	tmp
 }
 
-void	r_rboth()
+void	r_rboth(t_stack **a, t_stack **b)
 {
-	r_stack();
-	r_stack();
+	r_stack(a);
+	r_stack(b);
 }
 
-void	rr_stack()
+void	rr_stack(t_stack **a)
 {
 
 }
 
-void	rr_rboth()
+void	rr_rboth(t_stack **a, t_stack **b)
 {
-	rr_stack();
-	rr_stack();
+	rr_stack(a);
+	rr_stack(b);
 }
