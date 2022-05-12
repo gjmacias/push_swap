3eb5af6b2e0c64365e15b0c6fa40729a8339def4
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2022-05-12 10:07:34 +0200

    Update operations.c

--- operations.c
+++ operations.c
@@ -1,18 +1,41 @@
 #include "push_swap.h"
 
-void	p_stack()
+void	p_stack(t_stack **a, t_stack **b)
 {
-	
+	t_stack	tmp;
+
+	tmp = (t_stack *) mallloc(sizeof(t_stack));
+	if (tmp == NULL)
+		return (NULL);
+	if (*b == NULL)
+	{
+		(*b) = (t_stack *) mallloc(sizeof(t_stack));
+		if ((*b) == NULL)
+			return (NULL);
+		(*b)->number = (*a)->number;
+		(*b)->next = NULL;
+		tmp = (*a);
+		(*a) = (*a)->next;
+	}
+	else
+	{
+		tmp->number = (*a)->number;
+		tmp->next = NULL;
+		add_front(b, tmp);
+		tmp = (*a);
+		(*a) = (*a)->next;
+	}
+	free(tmp);
 }
 
 void	s_stack(t_stack **top)
 {
 	int	tmp;
 
-	if ((*top) && (*top->next))
+	if ((*top) && ((*top)->next))
 	{
-		tmp = *top->number;
-		*top->number = *top->next->number;
-		*top->next->number = tmp;
+		tmp = (*top)->number;
+		(*top)->number = (*top)->next->number;
+		(*top)->next->number = tmp;
 	}
 }
